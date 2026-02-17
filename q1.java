9ab0970fe413db992ca4a28ff0f8a1b149a59f5a
Syed Hunzala Ahmed Bukhari <k253089@nu.edu.pk>
2026-02-17 22:32:13 +0500

    q1

---
+++ q1.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+class Box{
+    private double width;
+    private double height;
+
+    public void print(){
+        System.out.println("Width: " + width + "\nHeight: " + height);
+    }
+
+    public Box(double width, double height){
+        if(width > 0 && height > 0){
+            this.width=width;
+            this.height=height;
+        } else{
+            System.out.println("Error: width and height must be positive");
+        }
+    }
+
+    public Box(double height){
+        this.height=height;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter width: ");
+        this.width = sc.nextDouble();
+    }
+
+    public Box(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter width: ");
+        this.width = sc.nextDouble();
+        System.out.println("Enter height: ");
+        this.height = sc.nextDouble();
+    }
+}
+
+public class Main
+{
+	public static void main(String[] args) {
+		
+        Box b3 = new Box();
+
+        
+        b3.print();
+	}
+}
